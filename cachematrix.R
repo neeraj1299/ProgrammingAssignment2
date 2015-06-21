6c59148bd2b5d9c21be597de2f6eb2e4223ee275
Neeraj Aggarwal <naggarwa@kcg.com>
2015-06-20 20:59:09 -0400

    added code to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,26 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    validate_matrix  <- function(x) {
+        if (  nrow(x) != 2 || ncol(x) != 2 )
+        {
+            message("Error:matrix not 2X2")
+            x <- NULL
+            return FALSE
+        } 
+        x
+    }
+    setmatrix_inv <- function(y) {
+        x <<- y
+        m <<- solve(x )
+    }
+    getmatrix <- function() { validate_matrix(x)}
+    getmatrix_inv <- function() m
+    list( getmatrix = getmatrix,
+         setmatrix_inv = setmatrix_inv, 
+         getmatrix_inv = getmatrix_inv,
+         validate_matrix = validate_matrix)     
 }
 
 
@@ -12,4 +31,28 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getmatrix_inv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$getmatrix()
+    if(is.null(data)) {
+        message("Error getting matrix inverse")
+          
+    } else {
+        m <- x$setmatrix_inv( data )
+        m
+    }
 }
+
+##
+## To test 
+## a<- makeCacheMatrix( matrix(1:6,3,3))
+## cacheSolve(a)
+##
+## a<- makeCacheMatrix( matrix(1:4,2,2))
+## cacheSolve(a)
+## cacheSolve(a)
+##
+##
